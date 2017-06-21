e8da67d038fd545e83423f1d7dc2fd8f36662bb0
icebeam7 <luis.beltran@itcelaya.edu.mx>
2017-06-21 23:51:01 +0200

    Agregar archivos de proyecto.

---
+++ VeterinariaWebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace VeterinariaWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ VeterinariaWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VeterinariaWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
